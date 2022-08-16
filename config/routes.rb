84df9135c8b4b0e8634b7db5885a5c84a7dc8d1a
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <lucas.mturra@gmail.com>
2022-08-16 15:56:46 -0300

    resolvido

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/edit/:id", to: "tasks#edit", as: "edit_task"
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: "task"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
